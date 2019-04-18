19176f8a88935fd827b982d27940fd0362568d3f
Thomas Bosley <tbosley100@gmail.com>
2019-04-18 16:19:09 -0400

    Added collectables and score

---
+++ Assets/CollectableItem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableItem : MonoBehaviour {
+	[SerializeField] private int m_Value;
+
+	private Inventory inventory;
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Player") {
+			inventory = other.GetComponent<Inventory>();
+			inventory.AddScore(m_Value);
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Inventory.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour {
+	[SerializeField] private int m_Score;
+	[SerializeField] private int m_Health;
+	[SerializeField] private GameObject m_ScoreObject;
+
+	public void AddScore(int value) {
+		m_Score += value;
+	}
+
+	private void Update() {
+		m_ScoreObject.GetComponent<Text>().text = string.Format("Score: {0}", m_Score);
+	}
+}
